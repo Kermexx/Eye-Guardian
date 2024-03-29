81a613a25e8f8f5d320658c894e21151764a8c6b
Lucas <145804220+Kermexx@users.noreply.github.com>
2024-03-29 10:52:10 -0300

    Update Código_sem_IA.py

--- Código_sem_IA.py
+++ Código_sem_IA.py
@@ -814,7 +814,8 @@ LEMBRE-SE DE SEMPRE SALVAR AS CONFIGURAÇÕES!!!
         wb.save(excel_file)
 
     def graphic(self):
-        messagebox.showinfo("DICA", "O gráfico que lista o caminho dos diretórios é separado em cores, onde: Azul - TOP 1, Amarelo - TOP 2, Verde - TOP 3!")
+        messagebox.showinfo("DICA",
+                            "O gráfico que lista o caminho dos diretórios é separado em cores, onde: Azul - TOP 1, Amarelo - TOP 2, Verde - TOP 3!")
         if not os.path.exists("resultado_scan.xlsx"):
             messagebox.showwarning("Aviso", "O arquivo 'resultado_scan.xlsx' não existe.")
             return
@@ -837,10 +838,18 @@ LEMBRE-SE DE SEMPRE SALVAR AS CONFIGURAÇÕES!!!
         latest_time = data['Data'].max()
         latest_data = data[data['Data'] == latest_time]
 
-        # Preparamos o conjunto de dados para o total de informações encontradas no conjunto mais recente
-        total_info_latest = latest_data['Informação encontrada'].value_counts()
+        # Atualiza o campo 'Diretório' para refletir o caminho da pasta, não do arquivo
+        latest_data['Diretório'] = latest_data['Diretório'].apply(lambda x: str(Path(x).parent))
 
-        # Definindo o tamanho da figura para 1200x700 pixels
+        # Preparando o conjunto de dados com a contagem de informações por diretório
+        directory_info_counts = latest_data.groupby('Diretório')['Informação encontrada'].count().sort_values(
+            ascending=False)
+        top_directories = directory_info_counts.head(3)
+        total_info = directory_info_counts.sum()
+        directory_percents = (top_directories / total_info) * 100
+        other_percent = 100 - directory_percents.sum()
+
+        # Definindo o tamanho da figura
         desired_width_px = 1200
         desired_height_px = 700
         dpi = plt.rcParams.get('figure.dpi')
@@ -851,35 +860,32 @@ LEMBRE-SE DE SEMPRE SALVAR AS CONFIGURAÇÕES!!!
         def wrap_labels(labels, width=30):
             return ['\n'.join(textwrap.wrap(label, width=width)) for label in labels]
 
-        # Configuramos a figura e os eixos para os subplots
+        # Gráfico geral de informações sensíveis encontradas
+        total_info_latest = latest_data['Informação encontrada'].value_counts()
         wrapped_labels_total_info = wrap_labels(total_info_latest.index)
         ax1 = fig.add_subplot(2, 2, 2)
         ax1.pie(total_info_latest, labels=wrapped_labels_total_info, autopct='%1.1f%%', startangle=90)
-        ax1.set_title('Total de Informações Sensíveis Encontradas', fontsize=10, fontweight='bold')
+        ax1.set_title('Total de Informações Sensíveis Encontradas - (Todos diretórios)', fontsize=10, fontweight='bold')
 
-        # Calculando a porcentagem de informações sensíveis para cada diretório
-        total_directories_info = latest_data['Diretório'].value_counts()
-        total_info = total_directories_info.sum()
-        top_directories = total_directories_info.head(3)
-        directory_percents = (top_directories / total_info) * 100
-        other_percent = 100 - directory_percents.sum()
-
-        # Concatenando os dados dos diretórios com o valor de "Outros"
+        # Gráfico dos TOP 3 diretórios
+        pie_labels = ['TOP 1 - ' + top_directories.index[0], 'TOP 2 - ' + top_directories.index[1],
+                      'TOP 3 - ' + top_directories.index[2], 'Outros']
         pie_data = pd.concat([directory_percents, pd.Series([other_percent], index=['Outros'])])
-        wrapped_labels_pie_data = wrap_labels(pie_data.index)
+        wrapped_labels_pie_data = wrap_labels(pie_labels)
 
-        # Adicionando o gráfico de comparação total
         ax_total_comparison = fig.add_subplot(2, 2, 1)
-        ax_total_comparison.pie(pie_data, labels=wrapped_labels_pie_data, autopct='%1.1f%%', startangle=90, labeldistance=1.3)
-        ax_total_comparison.set_title('Total de Informações Sensíveis por Diretório', fontsize=10, fontweight='bold')
+        ax_total_comparison.pie(pie_data, labels=wrapped_labels_pie_data, autopct='%1.1f%%', startangle=90,
+                                labeldistance=1.3)
+        ax_total_comparison.set_title('TOP 3 Diretórios mais sensíveis', fontsize=10, fontweight='bold')
 
-        # Gráficos de pizza para cada diretório principal com os dados mais recentes
+        # Gráficos detalhados para cada um dos TOP 3 diretórios
         for i, directory in enumerate(top_directories.index, start=1):
-            info = latest_data[latest_data['Diretório'] == directory]['Informação encontrada'].value_counts()
+            specific_data = latest_data[latest_data['Diretório'] == directory]
+            info = specific_data['Informação encontrada'].value_counts()
             wrapped_labels_info = wrap_labels(info.index)
-            ax = fig.add_subplot(2, 3, i + 3)  # Isso vai posicionar os subplots na parte inferior da figura
+            ax = fig.add_subplot(2, 3, i + 3)
             ax.pie(info, labels=wrapped_labels_info, autopct='%1.1f%%', startangle=90)
-            ax.set_title(f'Top {i} diretório mais sensível', fontsize=10, fontweight='bold')
+            ax.set_title(f'TOP {i} - Diretório Sensível', fontsize=10, fontweight='bold')
 
         plt.tight_layout()
         plt.show()
